ff08c62024724661cae110b0d07b8bb545b9ac52
Arda Beyazoglu <arda@beyazoglu.com>
2025-09-24 11:57:48 +0200

    fix: minor

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { shared } from '@myapp/shared/lib/shared';
+import { shared } from 'shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
